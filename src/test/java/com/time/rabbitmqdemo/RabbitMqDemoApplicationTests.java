7bf4d86a7f4bdd7c21b2ac574b87525d7ea0e7a2
Patiphon <tptpatiphon@gmail.com>
2023-07-10 16:57:53 +0700

    Init

---
+++ src/test/java/com/time/rabbitmqdemo/RabbitMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.rabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
